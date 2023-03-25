ff302c316c2115c932064f9cd48d39086ba6c04f
Ben Leech <b.leech@hotmail.ca>
2023-03-25 10:15:52 -0600

    mock land card

--- src/app/components/land-card/land-card.component.ts
+++ src/app/components/land-card/land-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {LandSummary} from "../../models/land-summary.model";
 
 @Component({
   selector: 'app-land-card',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./land-card.component.scss']
 })
 export class LandCardComponent {
+
+  @Input() landSummary: LandSummary;
 
 }
