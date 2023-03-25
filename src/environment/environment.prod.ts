5d6583e8e998d3e086f4e7263a5ef083116ac932
Ben Leech <b.leech@hotmail.ca>
2023-03-25 16:03:13 -0600

    fixed extra slash

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  bffUrl: 'https://gc.rawley.xyz/',
+  bffUrl: 'https://gc.rawley.xyz',
   googleClientId: '953637215774-de5fr3c8dj9mbrvr4ccptlvd4ss3s6u9.apps.googleusercontent.com'
 };
